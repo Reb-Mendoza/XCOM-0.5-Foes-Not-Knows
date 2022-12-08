19af44fc5318c1c7f498821149f21af413409eb0
Reb Mendoza <rebisaguava@gmail.com>
2022-12-08 05:37:13 -0500

    update

--- gamecode.js
+++ gamecode.js
@@ -1,4 +1,164 @@
-function square(x) {
-    return x * x
+let turnStep = 0;
+let operator = {ID: [1], HP: [], X: [2], Y: [3]};
+let alien = {ID: [], HP: [], X: [], Y: []};
+//Vertical walls are described by the coordinate to the left of the wall. Horizontal walls are described by the coordinate below the wall.
+let wall = {vertical: {X: [], Y:[]}, horizontal: {X: [], Y:[]}};
+let smoke = {X: [], Y:[]};
+
+
+//Returns the absolute value of a number input. Output: Number
+function abs(x) {
+    if (x < 0) {
+        x = x * -1
+    }
+    return x
 }
-console.log(square(4))
+
+//Returns the distance between two number inputs. Output Number
+function dist(x,targetX) {
+    return abs(targetX - x);
+}
+
+//Returns what team or what unit ID is on a given tile. Output: String
+function checkTile(x,y,type) {
+    var faction
+    var ID
+    for (i=0; i < operator.X.length; i++) {
+        checkX = (operator.X[i]);
+        for (j=0; j < operator.Y.length; j++) {
+            checkY = (operator.Y[j]);
+            if (checkX == x && checkY == y) {
+                faction = "operator";
+                ID = j+1;
+            }
+        }
+    }
+    for (i=0; i < alien.X.length; i++) {
+        checkX = (alien.X[i]);
+        for (j=0; j < alien.Y.length; j++) {
+            checkY = (alien.Y[j]);
+            if (checkX == x && checkY == y) {
+                faction = "operator";
+                ID = j+1;
+            }
+        }
+    }
+    if (type == "faction") {
+        return faction;
+    } else if (type == "ID") {
+        return ID;
+    }
+}
+
+//Returns whether or not there is a wall on a tile, given a position and a direction. Output: Boolean
+function checkIfWall(x,y,direction) {
+    if (direction == "vertical") {
+        for (i=0; i < wall.vertical.X; i++) {
+            if ((wall.vertical.X[i] == x) && (wall.vertical.Y[i] == y)) {
+                return true;
+            }
+        }
+        return false;
+    } else if (direction == "horizontal") {
+        for (i=0; i < wall.horizontal.X; i++) {
+            if ((wall.horizontal.X[i] == x) && (wall.horizontal.Y[i] == y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//Returns whether or not there is a line of fire between two points given an initial position, a target position, and a sight range. Output: Boolean
+function canHit(x,y,targetX,targetY,range) {
+    if ((dist(x,targetX) > 1) && (dist(y,targetY) > 1)) {
+        return false;
+    } else if ((dist(x,targetX) <= range) && (dist(y,targetY) <= range)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Returns whether or not there is a line of sight between two points given an initial position, a target position, and a sight range. Output: Boolean
+//This differs from canHit in that it considers how walls and smoke block vision between those two points.
+function canSee(x,y,targetX,targetY,range) {
+    let hit = true;
+    if (canHit(x,y,targetX,targetY,range) == true) {
+        //Corner case.
+        if ((dist(x,targetX) == 1) && (dist(y,targetY) == 1)) {
+            let verticalCheck = true;
+            let horizontalCheck = true;
+            let verticalVar = 0;
+            let horizontalVar = 0;
+            if (x > targetX) {
+                verticalVar = -1;
+            }
+            if (y > targetY) {
+                horizontalVar = -1;
+            }
+            if ((checkIfWall((x+verticalVar),(y+((horizontalVar*2)+1)),"vertical")) == true) {
+                verticalCheck = false;
+            }
+            if ((checkIfWall((x+((horizontalVar*2)+1)),(y+horizontalVar),"horizontal")) == true) {
+                horizontalCheck = false;
+            }
+            if ((horizontalCheck == false) && (verticalCheck == false)) {
+                hit = false;
+            }
+        //Straight line case.
+        } else if ((x == targetX) || (y == targetY)) {
+            if (x == targetX) {
+                if (y < targetY) {
+                    for (i=0; i < dist(y,targetY); i++) {
+                        if (checkIfWall(x,y+i) == true) {
+                            hit = false;
+                        }
+                    }
+                }
+                if (y > targetY) {
+                    for (i=0; i < dist(y,targetY); i++) {
+                        if (checkIfWall(x,) == true) {
+                            hit = false;
+                        }
+                    }
+                }
+            } else if (y == targetY) {
+                for (i=0; i < wall.vertical.Y; i++) {
+                    if ((wall.vertical.Y[i]) == y) {
+                        if (((x < targetX) && ((wall.vertical.X[i]) >= x) && ((wall.vertical.X[i]) < targetX)) || ((x > targetX) && ((wall.vertical.X[i]) < x) && ((wall.vertical.X[i]) >= targetX))) {
+                            hit = false;
+                        }
+                    }
+                }
+            }
+        //Skewed shot case.
+        } else {
+            var varVertical = 0;
+            var varHorizontal = 0;
+            if (dist(x,targetX) > 1) {
+                if (y < targetY) {
+                    varVertical = 1;
+                } else if (y > targetY) {
+                    varVertical = -1;
+                    varHorizontal = -1;
+                }
+                if (x < targetX) {
+                    for (i=0; i < dist(x,targetX); i++) {
+                        if ()
+                    }
+                } else if (x > targetX) {
+
+                }
+                
+            }
+        }
+    } else {
+        return false;
+    }
+}
+
+//This function is called when a unit is meant to be killed. If it's an alien, it is removed from the game. If it's an operator, they enter a DBNO state.
+function kill(x,y) {
+    
+}
