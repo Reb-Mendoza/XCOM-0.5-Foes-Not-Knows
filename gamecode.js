b92cb831ed5e500f55bf8abd5eadc70c26aac352
Reb Mendoza <rebisaguava@gmail.com>
2022-12-14 06:28:10 -0500

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -274,6 +274,7 @@ function move(x,y,direction) {
     console.log("move1")
     if (faction == "operator") {
         unit = document.querySelector("#operator" + index.toString());
+        circle = document.querySelector("#selectionCircle");
     } else if (faction == "alien") {
         unit = document.querySelector("#alien" + index.toString())
     }
@@ -288,6 +289,7 @@ function move(x,y,direction) {
         }
         unit.object3D.position.z += 2;
         if (faction == "operator") {
+            circle.object3D.position.z += 2;
         }
     } else if ((direction == "down") && !(checkIfWall(x,y-1,"horizontal"))) {
         if (faction == "operator") {
