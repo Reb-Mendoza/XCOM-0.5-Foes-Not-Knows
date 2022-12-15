007ef7ad286aa2483c6f32651b797765668f6484
Reb Mendoza <rebisaguava@gmail.com>
2022-12-15 04:21:46 -0500

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -30,27 +30,27 @@ function startGame(map) {
     for (i=0; i < wall.vertical.X.length; i++) {
         var sceneEl = document.querySelector("a-scene");
         var entityEl = document.createElement("a-entity");
-        sceneEl.append(entityEl);
+        sceneEl.appendChild(entityEl);
         entityEl.setAttribute("mixin", "hardWall");
-        entityEl.setAttribute("position", {x: (wall.vertical.X[i] * -2)+2, y: -4.5, z: (wall.vertical.Y[i] * 2)});
+        entityEl.setAttribute("position", {x: (wall.vertical.X[i] * -2)+2, y: -4.5, z: (wall.vertical.Y[i] * 2)-3});
         entityEl.setAttribute("rotation", {x: 0, y: 0, z: 0});
     }
     //Horizontal Walls.
     for (i=0; i < wall.horizontal.X.length; i++) {
         var sceneEl = document.querySelector("a-scene");
         var entityEl = document.createElement("a-entity");
-        sceneEl.append(entityEl);
+        sceneEl.appendChild(entityEl);
         entityEl.setAttribute("mixin", "hardWall");
-        entityEl.setAttribute("position", {x: (wall.horizontal.X[i] * -2)+1, y: -4.5, z: (wall.horizontal.Y[i] * 2)-1});
+        entityEl.setAttribute("position", {x: (wall.horizontal.X[i] * -2)+3, y: -4.5, z: (wall.horizontal.Y[i] * 2)-2});
         entityEl.setAttribute("rotation", {x: 0, y: 90, z: 0});
     }
     //Operators.
     for (i=0; i < operator.X.length; i++) {
         var sceneEl = document.querySelector("a-scene");
         var entityEl = document.createElement("a-entity");
-        sceneEl.append(entityEl);
+        sceneEl.appendChild(entityEl);
         entityEl.setAttribute("mixin", "operator");
-        entityEl.setAttribute("position", {x: (operator.X[i] * -2), y: -4.5, z: (operator.Y[i] * 2)+1});
+        entityEl.setAttribute("position", {x: (operator.X[i] * -2), y: -4.5, z: (operator.Y[i] * 2)});
         entityEl.setAttribute("unit", "number", (i+1));
     }
 }
